1d27b1104762ebb45ebbef1f0adb5ece246030a5
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-03-21 22:47:06 +0900

    feat: Location 모델 생성

---
+++ TheMiddle/Model/Location/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  TheMiddle
+//
+//  Created by 조호준 on 3/21/24.
+//
+
+import Foundation
+
+struct Location: Hashable {
+  var name: String
+  var latitude: Double
+  var longitude: Double
+}
