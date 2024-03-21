7e64bcfadb47cc88f936a4a7c4fe509837e574cc
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-03-21 16:51:10 +0900

    feat: 프로젝트 생성

---
+++ TheMiddle/App/TheMiddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMiddleApp.swift
+//  TheMiddle
+//
+//  Created by 조호준 on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMiddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheMiddle/Feature/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheMiddle
+//
+//  Created by 조호준 on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
