25e89da84b9b02f5a39ad82a8b79344bf93dd19a
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-03-24 15:21:03 +0900

    chore: 컨벤션 수정

--- TheMiddle/Model/Location/Location.swift
+++ TheMiddle/Model/Location/Location.swift
@@ -11,4 +11,14 @@ struct Location: Hashable {
   var name: String
   var latitude: Double
   var longitude: Double
+  
+  init(
+    name: String,
+    latitude: Double,
+    longitude: Double
+  ) {
+    self.name = name
+    self.latitude = latitude
+    self.longitude = longitude
+  }
 }
