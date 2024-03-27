423d5a7c76c08e58fd3b67b66bce2cffb8eba452
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-03-27 21:19:57 +0900

    refactor: Location의 인스턴스 추적을 위해 클래스로 수정

--- TheMiddle/Model/Location/Location.swift
+++ TheMiddle/Model/Location/Location.swift
@@ -7,18 +7,44 @@
 
 import Foundation
 
-struct Location: Hashable {
+final class Location {
   var name: String
+  var category: String
+  var address: String
+  var roadAddress: String
   var latitude: Double
   var longitude: Double
   
   init(
     name: String,
+    category: String,
+    address: String,
+    roadAddress: String,
     latitude: Double,
     longitude: Double
   ) {
     self.name = name
+    self.category = category
+    self.address = address
+    self.roadAddress = roadAddress
     self.latitude = latitude
     self.longitude = longitude
+  }
+}
+
+extension Location: Hashable {
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name)
+    hasher.combine(category)
+    hasher.combine(address)
+    hasher.combine(roadAddress)
+    hasher.combine(latitude)
+    hasher.combine(longitude)
+  }
+  
+  static func == (lhs: Location, rhs: Location) -> Bool {
+    return lhs.name == rhs.name &&
+    lhs.latitude == rhs.latitude &&
+    lhs.longitude == rhs.longitude
   }
 }
