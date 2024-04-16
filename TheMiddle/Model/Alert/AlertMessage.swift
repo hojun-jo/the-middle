828559ab8d5e64da1c8b00ade9602c01a20c0083
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-04-16 21:43:44 +0900

    feat: 매직 리터럴 치환을 위한 AlertMessage 모델 생성

---
+++ TheMiddle/Model/Alert/AlertMessage.swift
@@ -0,0 +1,15 @@
+//
+//  AlertMessage.swift
+//  TheMiddle
+//
+//  Created by 조호준 on 4/16/24.
+//
+
+enum AlertMessage: String {
+  // MARK: - HomeView
+  case needStartLocation = "출발지를 추가해 주세요."
+  
+  // MARK: - MapView
+  case needSearchLocation = "검색 장소를 입력해주세요."
+  case canNotDisplayAllStartLocation = "모든 출발지를 표시할 수 없습니다."
+}
