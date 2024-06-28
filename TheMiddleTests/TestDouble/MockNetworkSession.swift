277be0b6217a7b6136a734167177becba2acf64b
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-06-28 18:30:20 +0900

    test: MockNetworkSession 추가

---
+++ TheMiddleTests/TestDouble/MockNetworkSession.swift
@@ -0,0 +1,21 @@
+//
+//  MockNetworkSession.swift
+//  TheMiddleTests
+//
+//  Created by 조호준 on 6/28/24.
+//
+
+import Foundation
+@testable import TheMiddle
+
+final class MockNetworkSession: NetworkSessionProtocol {
+  func data(
+    for request: URLRequest,
+    delegate: (URLSessionTaskDelegate)? = nil
+  ) async throws -> (Data, URLResponse) {
+    let dummy: Data = try JSONEncoder().encode(dummyKakaoLocation)
+    let response: URLResponse = .init()
+    
+    return (dummy, response)
+  }
+}
