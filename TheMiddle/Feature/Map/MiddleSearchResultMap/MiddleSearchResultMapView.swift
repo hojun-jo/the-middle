43259b6f680bf4fe1adf6d8eab97ef10f6fdf72f
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-08-09 20:57:40 +0900

    feat: MiddleSearchResultMapView 생성

---
+++ TheMiddle/Feature/Map/MiddleSearchResultMap/MiddleSearchResultMapView.swift
@@ -0,0 +1,18 @@
+//
+//  MiddleSearchResultMapView.swift
+//  TheMiddle
+//
+//  Created by 조호준 on 8/9/24.
+//
+
+import SwiftUI
+
+struct MiddleSearchResultMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiddleSearchResultMapView()
+}
